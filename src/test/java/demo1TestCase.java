7e7250cd1dddb1a4239c734fb96c95e92ce15294
shikaifeng <kaifeng15@163.com>
2017-12-05 14:25:54 +0800

    api demo first commit

---
+++ src/test/java/demo1TestCase.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+import org.testng.*;
+
+
+public class demo1TestCase{
+        @Test
+        public void testcase1(){
+           Assert.assertTrue(true);
+           System.out.println("testcase1");
+        }
+        @Test
+        public void testcase2(){
+           Assert.assertTrue(true);
+           System.out.println("testcase1");
+        }
+}
